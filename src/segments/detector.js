f406e2205e666b449b704ec20a1c7c4d9fad8c64
Mathias Toala <fetoala@outlook.com>
2025-10-12 19:01:51 -0500

    Update detector.js to handle two players

--- src/segments/detector.js
+++ src/segments/detector.js
@@ -62,55 +62,60 @@ function declareWinner(){
 
 export async function detectPose() {
     const poses = await detector.estimatePoses(video);
-    if(poses.length > 0) {
-        const keypoints = poses[0].keypoints;
-
-        const shoulder = keypoints.find( kp => kp.name === 'right_shoulder');
-        const elbow = keypoints.find( kp => kp.name === 'right_elbow');
-        const wrist = keypoints.find( kp => kp.name === 'right_wrist');
-
-        if(!shoulder || !wrist || !elbow || shoulder.score < 0.5 || wrist.score < 0.5 || elbow.score < 0.5){
-            console.warn("No se encontraron todos los keypoints. Omitiendo cálculo.")
-        }
-
-        else{
-            const angle = calculateAngle(shoulder, elbow, wrist);
-            const directionAngle = getArmDirectionAngle(shoulder, wrist);
-            console.log('Ángulo del codo: ', angle);
-            console.log('Angúlo de la dirección del brazo: ', directionAngle);
-
-            const validElbow = angle >= 55 && angle <= 135;
-            const validDirection = directionAngle >= -45 && directionAngle <= 45;
-            // Pretty much placeholder angles frfrf
-
-            if (gameActive && !winnerDeclared){
-                if(validDirection && validElbow) {
-                    const gunPose = detectGunPose(); // More pleace holderisisrs
-                    if (gunPose){
-                        if(currentCue === "Fuego"){
-                            declareWinner();
-                        }
-                        else {
-                            falseAttemps++;
-                            message.textContent = "Te adelantaste...";
-                            if(falseAttemps >= 2){
-                                message.textContent = "¡IMIPACIENTE!";
-                                winnerDeclared = true;
+    if(poses.length !== 2){
+        messageElement.textContent = "¡Se necesitan dos jugadores!"
+    }
+    else {
+        poses.forEach(pose => {
+            const keypoints = pose.keypoints;
+            const shoulder = keypoints.find( kp => kp.name === 'right_shoulder');
+            const elbow = keypoints.find( kp => kp.name === 'right_elbow');
+            const wrist = keypoints.find( kp => kp.name === 'right_wrist');
+            
+            if(!shoulder || !wrist || !elbow || shoulder.score < 0.5 || wrist.score < 0.5 || elbow.score < 0.5){
+                console.warn("No se encontraron todos los keypoints. Omitiendo cálculo.")
+            }
+    
+            else{
+                const angle = calculateAngle(shoulder, elbow, wrist);
+                const directionAngle = getArmDirectionAngle(shoulder, wrist);
+                console.log('Ángulo del codo: ', angle);
+                console.log('Angúlo de la dirección del brazo: ', directionAngle);
+    
+                const validElbow = angle >= 55 && angle <= 135;
+                const validDirection = directionAngle >= -45 && directionAngle <= 45;
+                // Pretty much placeholder angles frfrf
+    
+                if (gameActive && !winnerDeclared){
+                    if(validDirection && validElbow) {
+                        const gunPose = detectGunPose(); // More pleace holderisisrs
+                        if (gunPose){
+                            if(currentCue === "Fuego"){
+                                declareWinner();
                             }
+                            else {
+                                falseAttemps++;
+                                message.textContent = "Te adelantaste...";
+                                if(falseAttemps >= 2){
+                                    message.textContent = "¡IMIPACIENTE!";
+                                    winnerDeclared = true;
+                                }
+                            }
+                        } 
+                        else {
+                            message.textContent = "¿A eso llamas pistola?";
                         }
-                    } 
-                    else {
-                        message.textContent = "¿A eso llamas pistola?";
                     }
-                }
-                else {
-                    if (currentCue === "Fuego"){
-                        if (!validElbow){message.textContent = "¡Mala puntería!";}
-                        else if (!validDirection) {message.textContent = "¡A dónde apuntas!";}
+                    else {
+                        if (currentCue === "Fuego"){
+                            if (!validElbow){message.textContent = "¡Mala puntería!";}
+                            else if (!validDirection) {message.textContent = "¡A dónde apuntas!";}
+                        }
                     }
                 }
             }
-        }
+        });
+
     }
 
     requestAnimationFrame(detectPose);
